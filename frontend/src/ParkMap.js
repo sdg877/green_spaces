fe5e32f0b3f2d7295fe2e9aa4451f9a174a5047e
Sdg877 <sdrakegill@gmail.com>
2025-05-08 06:16:14 +0100

    Search function implemented.

--- frontend/src/ParkMap.js
+++ frontend/src/ParkMap.js
@@ -1,15 +1,34 @@
-const map = L.map('map').setView([51.5074, -0.1278], 11); // London centre
+import React, { useEffect } from "react";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '© OpenStreetMap contributors'
-}).addTo(map);
-
-fetch('http://127.0.0.1:5000/get_parks')
-  .then(response => response.json())
-  .then(parks => {
-    parks.forEach(park => {
-      const latlngs = park.coordinates.map(coord => [coord[0], coord[1]]);
-      L.polygon(latlngs, { color: 'green' }).addTo(map).bindPopup(park.name);
+function ParkMap({ parks }) {
+  useEffect(() => {
+    const map = L.map("map", {
+      center: [51.5074, -0.1278],
+      zoom: 11,
+      maxZoom: 18,
     });
-  })
-  .catch(err => console.error('Error loading park data:', err));
+
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution: "© OpenStreetMap contributors",
+    }).addTo(map);
+
+    map.eachLayer((layer) => {
+      if (layer instanceof L.Marker || layer instanceof L.Polygon) {
+        map.removeLayer(layer);
+      }
+    });
+
+    parks.forEach((park) => {
+      const latlngs = park.coordinates.map((coord) => [coord[0], coord[1]]);
+      L.polygon(latlngs, { color: "green" }).addTo(map).bindPopup(park.name);
+    });
+
+    return () => map.remove();
+  }, [parks]);
+
+  return <div id="map" style={{ height: "90vh", width: "100%" }}></div>;
+}
+
+export default ParkMap;
